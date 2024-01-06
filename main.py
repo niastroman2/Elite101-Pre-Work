920e67bda77ee826377dd716972d9c3a23400ee8
niastroman2 <155405129+niastroman2@users.noreply.github.com>
2024-01-06 23:36:49 +0000

    Finished chatbot

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("Hello! Welcome to Nia's ChatBot!")
+user_name = input("What is your name?: ")
+user_age = int(input("How old are you?: "))
+user_help = input("Hello! " + user_name + " what can I do for you today? \n1.Speak to a manager \n2.See new inventory \n3.Exit: ")
+if user_help == "Exit":
+  exit()
+
+
+
+
